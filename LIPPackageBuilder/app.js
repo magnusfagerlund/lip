90975ba4106d99327e84607cec70d4ead6347ffc
Kahrstrom <jonatan.kahrstrom@gmail.com>
2016-01-26 12:59:38 +0100

    Changes

--- LIPPackageBuilder/app.js
+++ LIPPackageBuilder/app.js
@@ -29,7 +29,37 @@ lbs.apploader.register('LIPPackageBuilder', function () {
     */
     self.initialize = function (node, vm) {
 
+        $('title').html('LIP');
 
+
+        vm.fieldTypes = {
+            "1" : "string",
+            "2" : "geography",
+            "3" : "integer",
+            "4" : "decimal",
+            "7" : "time",
+            "8" : "text",
+            "9" : "script",
+            "10" : "html",
+            "11" : "xml",
+            "12" : "link",
+            "13" : "yesno",
+            "14" : "multirelation",
+            "15" : "file",
+            "16" : "relation",
+            "17" : "user",
+            "18" : "security",
+            "19" : "calendar",
+            "20" : "set",
+            "21" : "option",
+            "22" : "image",
+            "23" : "formatedstring",
+            "25" : "automatic",
+            "26" : "color",
+            "27" : "sql",
+            "255" : "system"
+        }
+        // Attributes for tables
         vm.tableAttributes = [
             "tableorder",
             "invisible",
@@ -40,6 +70,7 @@ lbs.apploader.register('LIPPackageBuilder', function () {
             "actionpad"
         ];
 
+        // Attributes for fields
         vm.fieldAttributes = [
             "fieldtype",
             "limereadonly",
@@ -64,11 +95,9 @@ lbs.apploader.register('LIPPackageBuilder', function () {
             "limevalidationrule",
             "label",
             "adlabel"
-
         ]
-        $('title').html('LIP');
-
-
+        
+        // Checkbox to select all tables
         vm.selectTables = ko.observable(false);
         vm.selectTables.subscribe(function(newValue){
             ko.utils.arrayForEach(vm.filteredTables(),function(item){
@@ -76,40 +105,67 @@ lbs.apploader.register('LIPPackageBuilder', function () {
             });
         });
 
+        // Navbar function to change tab
         vm.showTab = function(t){
             vm.activeTab(t);
         }
 
+        // Set default tab to details
+        vm.activeTab = ko.observable("details");
+
+
+        // Serialize selected tables and fields and combine with localization data
         vm.serializePackage = function(){
             var data = {};
             var tables = [];
 
+            // For each selected table
             $.each(vm.selectedTables(),function(i,table){
                 
+                // Fetch local names from table with same name
                 var localNameTable  = vm.localNames.Tables.filter(function(t){
                     return t.name == table.name;
                 })[0];
 
+                // Set singular and plural local names for table
                 table.localname_singular = localNameTable.localname_singular;
                 table.localname_plural = localNameTable.localname_plural;
                 
+                // For each selected field in current table
                 var fields = [];
                 $.each(table.selectedFields(),function(j,field){
+                    // Fetch local names from field with same name
                     var localNameField = localNameTable.Fields.filter(function(f){
                         return f.name == field.name;
                     })[0];
-                
+                    
+                    // Set local names for current field
                     field.localname = localNameField;
                 
                     if(field.localname && field.localname.name)
                         delete field.localname.name;
 
+                    if(field.localname && field.localname.order)
+                        delete field.localname.order;
+
+                    if(field.separator && field.separator.order)
+                        delete field.separator.order;   
+
+                    if(field.localname && field.localname.option)
+                        delete field.localname.option;
+
+                    // Push field to fields
                     fields.push(field);
                 });
+
+                // Set fields to the current table
                 table.fields = fields;
+
+                // Push table to tables
                 tables.push(table);
             });
 
+            // Build package json from details and database structure
             data = {
                 "name": vm.name(),
                 "author": vm.author(),
@@ -126,22 +182,26 @@ lbs.apploader.register('LIPPackageBuilder', function () {
 
                 }
             }
+
+            // Save to file using microsofts weird ass self developedd file saving stuff
             var blobObject = new Blob([JSON.stringify(data)]); 
-            window.navigator.msSaveBlob(blobObject, (vm.name() || 'lippackage') + '.json')
+            window.navigator.msSaveBlob(blobObject, 'package.json')
         }
 
+        // Function to filter tables
         vm.filterTables = function(){
             if(vm.tableFilter() != ""){
                 vm.filteredTables.removeAll(); 
-                vm.filteredTables(ko.utils.arrayFilter(vm.tables(), function(item) {
 
+                // Filter on the three visible columns (name, localname, timestamp)
+                vm.filteredTables(ko.utils.arrayFilter(vm.tables(), function(item) {
                     if(item.name.toLowerCase().indexOf(vm.tableFilter().toLowerCase()) != -1){
                         return true;
                     }
                     if(item.localname.toLowerCase().indexOf(vm.tableFilter().toLowerCase()) != -1){
                         return true;
                     }
-                    if(item.timestamp.toLowerCase().indexOf(vm.tableFilter().toLowerCase()) != -1){
+                    if(item.timestamp().toLowerCase().indexOf(vm.tableFilter().toLowerCase()) != -1){
                         return true;
                     }
                     return false;
@@ -151,59 +211,74 @@ lbs.apploader.register('LIPPackageBuilder', function () {
             }
         }
 
-        var db = {};
-        lbs.loader.loadDataSource(db, { type: 'storedProcedure', source: 'csp_lip_getxmldatabase_wrapper', alias: 'structure' }, false);
-        vm.datastructure = db.structure.data;
+        // Filter observables
+        vm.tableFilter = ko.observable("");
+        vm.fieldFilter = ko.observable("");
 
-        vm.activeTab = ko.observable("details");
-
+        // Load databas structure
+        try{
+            var db = {};
+            lbs.loader.loadDataSource(db, { type: 'storedProcedure', source: 'csp_lip_getxmldatabase_wrapper', alias: 'structure' }, false);
+            vm.datastructure = db.structure.data;
+        }
+        catch(err){
+            alert(err)
+        }
+        // Data from details
         vm.author = ko.observable("");
         vm.comment = ko.observable("");
         vm.description = ko.observable("");
         vm.versionNumber = ko.observable("");
         vm.name = ko.observable("");
+        // Set default status to development
         vm.status = ko.observable("Development");
 
-        vm.tableFilter = ko.observable("");
-        vm.fieldFilter = ko.observable("");
+        // Set status options 
+        vm.statusOptions = ko.observableArray([
+            new StatusOption('Development'), new StatusOption('Beta'), new StatusOption('Release')
+        ]);
+        
+        // Load localization data
+        try{
+            var localData = {};
+            lbs.loader.loadDataSource(localData, { type: 'storedProcedure', source: 'csp_lip_getlocalnames', alias: 'localNames' }, false);
+            vm.localNames = localData.localNames.data;
+        }
+        catch(err){
+            alert(err)
+        }
+        // Table for which fields are shown
         vm.shownTable = ko.observable();
-        var localData = {};
-        lbs.loader.loadDataSource(localData, { type: 'storedProcedure', source: 'csp_lip_getlocalnames', alias: 'localNames' }, false);
-
-        vm.localNames = localData.localNames.data;
-       
+        // All tables loaded
         vm.tables = ko.observableArray();
+        // Filtered tables. These are the ones loaded into the view
         vm.filteredTables = ko.observableArray();
-
+        // Load model objects
         initModel(vm);
 
+        // Populate table objects
         vm.tables(ko.utils.arrayMap(vm.datastructure.table,function(t){
             return new Table(t);
         }));
   
+        // Computed with all selected tables
         vm.selectedTables = ko.computed(function(){
             return ko.utils.arrayFilter(vm.tables(), function(t){
                 return t.selected();
             });
         });
 
+        // Subscribe to changes in filters
         vm.fieldFilter.subscribe(function(newValue){ 
             vm.shownTable().filterFields();
         })
+        vm.tableFilter.subscribe(function(newValue){
+            vm.filterTables();
+        });
         
-
-        vm.statusOptions = ko.observableArray([
-            new Option('Development'), new Option('Beta'), new Option('Release')
-        ]);
-
-
-        
-
-        vm.tableFilter.subscribe(function(newValue){vm.filterTables()});
-
-        
-
+        // Set default filter
         vm.filterTables();
+
         return vm;
     };
 
--- LIPPackageBuilder/model.js
+++ LIPPackageBuilder/model.js
@@ -1,48 +1,66 @@
 
 var vm = {};
 
+// Load viewmodel
 initModel = function(viewModel){
 	vm = viewModel;
 }
 
+// Table object
 var Table = function(t){
 
     var self = this;
+    // Load database name
     self.name = t.name;
+    // Load local name
     self.localname = t.localname;
-    self.timestamp = moment(t.timestamp).format("YYYY-MM-DD");
+    // Load timestamp
+    self.timestamp = ko.observable(moment(t.timestamp).format("YYYY-MM-DD"));
+    // Load invisible attribute
     self.invisible = t.invisible;
+    // Initiate fields visible in gui
     self.guiFields = ko.observableArray();
 
+    // Load attributes 
     self.attributes = {};
     $.each(vm.tableAttributes, function(i, a){
         self.attributes[a] = t[a];
     });
 
+    // Load gui fields
     self.guiFields(ko.utils.arrayMap(ko.utils.arrayFilter(t.field,function(f){ return f.fieldtype != 255;}), function(f){
         return new Field(f, self.name);
     }));
 
+    // If table is selected
     self.selected = ko.observable(false);
+
+    // If fields of table are shown in column to the right
     self.shown = ko.computed(function(){
         return vm.shownTable() ? (vm.shownTable().name == self.name) : false; 
     });
 
+    // Click function to select table
     self.select = function(){
         self.selected(!self.selected());
     }
+
+    // Click function to show fields
     self.show = function(){
         vm.shownTable(vm.shownTable() ? (vm.shownTable().name == self.name ? null: self) : self);
     }
 
+    // Computed for keeping track of selected fields
     self.selectedFields = ko.computed(function(){
         return ko.utils.arrayFilter(self.guiFields(), function(f){
             return f.selected();
         });
     });
 
+    // Filtere fields. These are the ones shown in the gui (but based on guiFields)
     self.filteredFields = ko.observableArray();
 
+    // Filter function for fields
     self.filterFields = function(){
         if(vm.fieldFilter() != ""){
             self.filteredFields.removeAll(); 
@@ -54,7 +72,7 @@ var Table = function(t){
                 if(item.localname.toLowerCase().indexOf(vm.fieldFilter().toLowerCase()) != -1){
                     return true;
                 }
-                if(item.timestamp.toLowerCase().indexOf(vm.fieldFilter().toLowerCase()) != -1){
+                if(item.timestamp().toLowerCase().indexOf(vm.fieldFilter().toLowerCase()) != -1){
                     return true;
                 }
                 return false;
@@ -64,9 +82,10 @@ var Table = function(t){
         }
     }
 
+    // Select all fields
     self.selectFields = ko.observable(false);
 
-
+    // Subscribe to select all event
     self.selectFields.subscribe(function(newValue){
         ko.utils.arrayForEach(self.filteredFields(),function(item){
             item.selected(newValue);
@@ -74,24 +93,34 @@ var Table = function(t){
         self.selected(newValue);
     });
 
+    // Set default empty filter
     self.filterFields();
 }
 
 var Field = function(f, tablename){
     var self = this;
 
+    // Field attributes
     self.table = tablename;
     self.name = f.name;
-    self.timestamp = moment(f.timestamp).format("YYYY-MM-DD");
+    self.timestamp = ko.observable(moment(f.timestamp).format("YYYY-MM-DD"));
     self.localname = f.localname;
-    self.fieldtype = f.fieldtype;
-    self.selected = ko.observable(false);
+    // self.fieldtype = vm.fieldTypes[f.fieldtype];
 
     self.attributes = {};
     $.each(vm.fieldAttributes, function(i, a){
-        self.attributes[a] = f[a];
+        if(a == 'fieldtype'){
+            self.attributes[a] = vm.fieldTypes[f[a]];
+        }
+        else{
+            self.attributes[a] = f[a];
+        }
     });
 
+    // Observable for selecting field
+    self.selected = ko.observable(false);
+
+    // Subscribe to select event to see if table should be selected or deselected
     self.selected.subscribe(function(newValue){
         if(newValue){
             vm.shownTable().selected(newValue);
@@ -105,12 +134,14 @@ var Field = function(f, tablename){
             vm.shownTable().selected(checked);
         }
     })
+    // Click function for select
     self.select = function(){
         self.selected(!self.selected());
     }
 }
 
-var Option = function(o){
+// Status options (development, beta, release)
+var StatusOption = function(o){
     var self = this;
     self.text = o;
     this.select = function(){
