689e9402b4a8afad4726925b64c221000512a0ce
jonnyspringare <jonny.springare@lundalogik.se>
2016-07-11 13:08:30 +0200

    Update csp_lip_settableattributes.sql
    Extended error handling

--- sql/csp_lip_settableattributes.sql
+++ sql/csp_lip_settableattributes.sql
@@ -16,20 +16,29 @@ CREATE PROCEDURE [dbo].[csp_lip_settableattributes]
 	, @@label INT = NULL
 	, @@actionpad NVARCHAR(MAX) = NULL
 	, @@log BIT = NULL
-	, @@errorMessage NVARCHAR(512) OUTPUT
+	, @@errorMessage NVARCHAR(MAX) OUTPUT
+	, @@warningMessage NVARCHAR(MAX) OUTPUT
 AS
 BEGIN
 	-- FLAG_EXTERNALACCESS --
 	DECLARE	@return_value INT
+	DECLARE @linebreak NVARCHAR(2)
 	
 	SET @return_value =  NULL
 	SET @@errorMessage = N''
+	SET @@warningMessage = N''
+	SET @linebreak = CHAR(13) + CHAR(10)
 	
 	--Set descriptive expression
 	EXEC @return_value = [dbo].[lsp_setstring]
 		@@idstring = @@iddescriptiveexpression
 		, @@lang = N'ALL'
 		, @@string = @@descriptive
+		
+	IF @return_value <> 0
+	BEGIN
+		SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set descriptive expression for table ''' + @@tablename  + @linebreak
+	END
 	
 	--Set table order
 	EXEC @return_value = [dbo].[lsp_setattributevalue]
@@ -37,6 +46,11 @@ BEGIN
 		, @@idrecord = @@idtable
 		, @@name = N'tableorder'
 		, @@valueint = @@tableorder
+		
+	IF @return_value <> 0
+	BEGIN
+		SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set table order for table ''' + @@tablename  + @linebreak
+	END
 		
 	--Set invisible
 	IF @@invisible = 1 OR @@invisible = 2
@@ -46,6 +60,10 @@ BEGIN
 			, @@idrecord = @@idtable
 			, @@name = N'invisible'
 			, @@valueint = @@invisible
+		IF @return_value <> 0
+		BEGIN
+			SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set invisible attribute for table ''' + @@tablename  + @linebreak
+		END
 	END
 	
 	--Set comment
@@ -56,6 +74,11 @@ BEGIN
 			, @@idrecord = @@idtable
 			, @@name = N'syscomment'
 			, @@value = @@syscomment
+			
+		IF @return_value <> 0
+		BEGIN
+			SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set syscomment for table ''' + @@tablename  + @linebreak
+		END
 	END
 	
 	--Set label
@@ -66,6 +89,10 @@ BEGIN
 			, @@idrecord = @@idtable
 			, @@name = N'label'
 			, @@valueint = @@label
+		IF @return_value <> 0
+		BEGIN
+			SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set attribute ''label'' for table ''' + @@tablename  + @linebreak
+		END
 	END
 	
 	--Set actionpad
@@ -76,6 +103,10 @@ BEGIN
 			, @@idrecord = @@idtable
 			, @@name = N'actionpad'
 			, @@value = @@actionpad
+		IF @return_value <> 0
+		BEGIN
+			SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set actionpad for table ''' + @@tablename  + @linebreak
+		END
 	END
 	
 	--Set logging option
@@ -86,14 +117,13 @@ BEGIN
 			, @@idrecord = @@idtable
 			, @@name = N'log'
 			, @@value = @@log
+		IF @return_value <> 0
+		BEGIN
+			SET @@warningMessage = @@warningMessage + N'Warning: couldn''t set log attribute for table ''' + @@tablename  + @linebreak
+		END
 	END
 	
 	EXEC lsp_setdatabasetimestamp
 	EXEC lsp_refreshldc
 	
-	--If return value is not 0, something went wrong while setting table attributes
-	IF @return_value <> 0
-	BEGIN
-		SET @@errorMessage = N'Something went wrong while setting attributes for table ''' + @@tablename + N'''. Please check that table properties are correct.'
-	END
 END
